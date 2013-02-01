14f8e85ca1b3464aadc7af703ff5dd91531ae3e9
Anca <schumika@gmail.com>
2013-02-01 17:28:57 +0200

    updates on scores export image

--- ScorePad/AJExportHandler.m
+++ ScorePad/AJExportHandler.m
@@ -10,6 +10,12 @@
 
 #import <QuartzCore/QuartzCore.h>
 
+#import "AJScoresManager.h"
+
+#import "UIFont+Additions.h"
+#import "UIImage+Additions.h"
+#import "UIColor+Additions.h"
+
 @implementation AJExportHandler
 
 @synthesize game = _game;
@@ -32,13 +38,77 @@
 - (UIImage *)createPlayersImage {
     CGRect imageBounds = [[UIScreen mainScreen] bounds];
      
-    CGFloat calculatedHeight = self.game.players.count * 50.0 + 60.0;
+    CGFloat calculatedHeight = self.game.players.count * 50.0 + 70.0;
     imageBounds.size.height = MIN(calculatedHeight, imageBounds.size.height - [UIImage imageNamed:@"nav-bar.png"].size.height - 20.0);
     
     UIGraphicsBeginImageContextWithOptions(imageBounds.size, NO, 0.0);
     
     CGContextRef contextRef = UIGraphicsGetCurrentContext();
     CGContextDrawImage(contextRef, imageBounds, [UIImage imageNamed:@"background.png"].CGImage);
+    
+    // preparing for writing text
+    
+    //    CGContextTranslateCTM(contextRef, 0, imageBounds.size.height);
+    //    CGContextScaleCTM(contextRef, 1, -1);
+//    CGAffineTransform transform = CGAffineTransformMake(1.0, 0.0, 0.0, -1.0, 0.0, 0.0);
+//    CGContextSetTextMatrix(contextRef, transform);
+    CGContextSetTextDrawingMode(contextRef, kCGTextFill);
+    
+    // write Game name
+    NSString *gameName = self.game.name;
+	CGContextSetFillColorWithColor(contextRef, [UIColor colorWithHexString:self.game.color].CGColor);
+    CGFloat fontSize = 65.0;
+	CGContextSelectFont(contextRef, "LDBrushStroke", fontSize, kCGEncodingMacRoman);
+    CGContextSetFont(contextRef, CGFontCreateWithFontName((CFStringRef)[UIFont LDBrushFontWithSize:fontSize].fontName));
+    CGSize nameStringSize = [gameName sizeWithFont:[UIFont LDBrushFontWithSize:fontSize]];
+    CGRect nameStringRect = CGRectMake((imageBounds.size.width - nameStringSize.width)/2, 5.0, nameStringSize.width, 60.0);
+    [gameName drawInRect:nameStringRect withFont:[UIFont LDBrushFontWithSize:fontSize]];
+    
+    // draw separator line
+    CGContextDrawImage(contextRef, CGRectMake(0.0, 63.0, imageBounds.size.width, 2.0), [UIImage imageNamed:@"separator_new2.png"].CGImage);
+    
+    NSArray *players = [[AJScoresManager sharedInstance] getAllPlayersForGame:self.game];
+    for (AJPlayer *player in players) {
+        int playerIndex = [players indexOfObject:player];
+        
+        // reset
+//        CGAffineTransform transform = CGAffineTransformMake(-1.0, 0.0, 0.0, 1.0, 0.0, 0.0);
+//        CGContextSetTextMatrix(contextRef, transform);
+        
+        // draw player image
+        UIImage *playerImage = nil;
+        if (player.imageData == nil) {
+            playerImage  = [[UIImage defaultPlayerPicture] resizeToNewSize:CGSizeMake(50.0, 50.0)];
+        } else {
+            playerImage = [[UIImage imageWithData:player.imageData] resizeToNewSize:CGSizeMake(50.0, 50.0)];
+        }
+        CGContextDrawImage(contextRef, CGRectMake(10.0, 65.0 + (playerIndex * 50.0) + 7.0, 35.0, 35.0), [playerImage applyMask:[UIImage imageNamed:@"mask.png"]].CGImage);
+        
+        // draw separator line
+        CGContextDrawImage(contextRef, CGRectMake(0.0, 65.0 + (playerIndex * 50.0) + 48, imageBounds.size.width, 2.0), [UIImage imageNamed:@"separator_new2.png"].CGImage);
+        
+        
+        
+        NSString *playerName = player.name;
+        // write player name
+        CGContextSetFillColorWithColor(contextRef, [UIColor colorWithHexString:player.color].CGColor);
+        CGFloat playerFontSize = 50.0;
+        CGContextSelectFont(contextRef, "LDBrushStroke", playerFontSize, kCGEncodingMacRoman);
+        CGContextSetFont(contextRef, CGFontCreateWithFontName((CFStringRef)[UIFont LDBrushFontWithSize:playerFontSize].fontName));
+        CGSize nameStringSize = [playerName sizeWithFont:[UIFont LDBrushFontWithSize:playerFontSize]];
+        CGRect nameStringRect = CGRectMake(60.0, 65.0 + playerIndex * 50.0, nameStringSize.width, 50.0);
+        [playerName drawInRect:nameStringRect withFont:[UIFont LDBrushFontWithSize:playerFontSize]];
+        
+        NSString *playerScore = [NSString stringWithFormat:@"%g", [player totalScore]];
+        // write player score
+        CGContextSetFillColorWithColor(contextRef, [UIColor AJBrownColor].CGColor);
+        CGFloat scoreFontSize = 65.0;
+        CGContextSelectFont(contextRef, "LDBrushStroke", scoreFontSize, kCGEncodingMacRoman);
+        CGContextSetFont(contextRef, CGFontCreateWithFontName((CFStringRef)[UIFont LDBrushFontWithSize:scoreFontSize].fontName));
+        CGSize scoreStringSize = [playerScore sizeWithFont:[UIFont LDBrushFontWithSize:scoreFontSize]];
+        CGRect scoreStringRect = CGRectMake(imageBounds.size.width - scoreStringSize.width - 10.0, 65.0 + playerIndex * 50.0, scoreStringSize.width, 50.0);
+        [playerScore drawInRect:scoreStringRect withFont:[UIFont LDBrushFontWithSize:scoreFontSize]];
+    }
     
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
 	
