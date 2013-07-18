01ea0563aa7ed0c0b080e99e8b70e30b2c0a1979
Anca <schumika@gmail.com>
2013-07-18 18:14:56 +0300

    changed navigation buttons

--- ScorePad/UIBarButtonItem+Additions.m
+++ ScorePad/UIBarButtonItem+Additions.m
@@ -12,25 +12,68 @@
 
 @implementation UIBarButtonItem (Additions)
 
+//+ (UIBarButtonItem *)clearBarButtonItemWithTitle:(NSString *)title target:(id)target action:(SEL)action {
+//    
+//    UIBarButtonItem *barButtonItem = nil;
+//    
+//    UIImage *buttonImage = [[UIImage imageNamed:@"bar-button.png"] stretchableImageWithLeftCapWidth:7.0 topCapHeight:15.0];
+//    
+//    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+//    [button setBackgroundImage:buttonImage forState:UIControlStateNormal];
+//    [button setTitleShadowColor:[UIColor whiteColor] forState:UIControlStateNormal];
+//    [button setTitleColor:[UIColor AJBrownColor] forState:UIControlStateNormal];
+//    [button setTitle:title forState:UIControlStateNormal];
+//    [button setTitleColor:[UIColor AJBrownColor] forState:UIControlStateNormal];
+//    button.titleLabel.font = [UIFont LDBrushFontWithSize:35.0];
+//    [button.titleLabel setShadowOffset:CGSizeMake(0, -1)];
+//    [button.titleLabel setFont:[UIFont fontWithName:@"Thonburi-Bold" size:17.0]];
+//    [button addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+//    CGSize buttonSize = [title sizeWithFont:button.titleLabel.font];
+//    CGFloat marginSpace = 20.0f;
+//    button.frame = CGRectMake(0, 0, buttonSize.width + marginSpace, 30);
+//    
+//    barButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+//    
+//    return barButtonItem;
+//}
+
+//+ (UIBarButtonItem *)backButtonItemWithTarget:(id)target action:(SEL)action {
+//    
+//    UIBarButtonItem *barButtonItem = nil;
+//    
+//    UIImage *backImage = [[UIImage imageNamed:@"back-button"] stretchableImageWithLeftCapWidth:15.0 topCapHeight:15.0];
+//    
+//    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+//    [button setBackgroundImage:backImage forState:UIControlStateNormal];
+//    [button setTitleShadowColor:[UIColor blackColor] forState:UIControlStateNormal];
+//    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+//    [button setTitle:@"Back" forState:UIControlStateNormal];
+//    [button setTitleEdgeInsets:UIEdgeInsetsMake(0.0, 4.0, 0.0, -4.0)];
+//    [button.titleLabel setShadowOffset:CGSizeMake(0, -1)];
+//    [button.titleLabel setFont:[UIFont fontWithName:@"Thonburi-Bold" size:17.0]];
+//    CGSize buttonSize = [@"Back" sizeWithFont:button.titleLabel.font];
+//    [button addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+//    CGFloat marginSpace = 20.0f;
+//    button.frame = CGRectMake(0, 0, buttonSize.width + marginSpace, 30);
+//    
+//    barButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+//    
+//    return barButtonItem;
+//}
+
 + (UIBarButtonItem *)clearBarButtonItemWithTitle:(NSString *)title target:(id)target action:(SEL)action {
     
     UIBarButtonItem *barButtonItem = nil;
     
-    UIImage *buttonImage = [[UIImage imageNamed:@"bar-button.png"] stretchableImageWithLeftCapWidth:7.0 topCapHeight:15.0];
-    
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [button setBackgroundImage:buttonImage forState:UIControlStateNormal];
     [button setTitleShadowColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor AJBrownColor] forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor AJGreenColor] forState:UIControlStateNormal];
     [button setTitle:title forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor AJBrownColor] forState:UIControlStateNormal];
     button.titleLabel.font = [UIFont LDBrushFontWithSize:35.0];
     [button.titleLabel setShadowOffset:CGSizeMake(0, -1)];
-    [button.titleLabel setFont:[UIFont fontWithName:@"Thonburi-Bold" size:17.0]];
     [button addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
     CGSize buttonSize = [title sizeWithFont:button.titleLabel.font];
-    CGFloat marginSpace = 20.0f;
-    button.frame = CGRectMake(0, 0, buttonSize.width + marginSpace, 30);
+    button.frame = CGRectMake(0, 0, buttonSize.width, 30);
     
     barButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
     
@@ -41,20 +84,13 @@
     
     UIBarButtonItem *barButtonItem = nil;
     
-    UIImage *backImage = [[UIImage imageNamed:@"back-button"] stretchableImageWithLeftCapWidth:15.0 topCapHeight:15.0];
+    UIImage *backImage = [UIImage imageNamed:@"back"];
     
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     [button setBackgroundImage:backImage forState:UIControlStateNormal];
-    [button setTitleShadowColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [button setTitle:@"Back" forState:UIControlStateNormal];
-    [button setTitleEdgeInsets:UIEdgeInsetsMake(0.0, 4.0, 0.0, -4.0)];
-    [button.titleLabel setShadowOffset:CGSizeMake(0, -1)];
-    [button.titleLabel setFont:[UIFont fontWithName:@"Thonburi-Bold" size:17.0]];
-    CGSize buttonSize = [@"Back" sizeWithFont:button.titleLabel.font];
     [button addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
-    CGFloat marginSpace = 20.0f;
-    button.frame = CGRectMake(0, 0, buttonSize.width + marginSpace, 30);
+    CGSize buttonSize = backImage.size;
+    button.frame = (CGRect){CGPointZero, buttonSize};
     
     barButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
     
